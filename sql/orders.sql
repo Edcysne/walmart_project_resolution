f534ffee67ddf84167a49856500760b90d710e4b
Edcysne <edcysne@gmail.com>
2025-10-08 13:44:14 +0100

    New sql order query

--- sql/orders.sql
+++ sql/orders.sql
@@ -1,16 +1,18 @@
 SELECT 
-    t1.date,
-    t1.order_id,
-    t1.order_amount,
-    t1.region,
-    t1.items_delivered,
-    t1.items_missing,
-    t1.delivery_hour,
-    t1.customer_name,
-    t1.customer_age,
-    t1.driver_name,
-    t1.age as driver_age
-FROM
-(SELECT * FROM orders_data o
+    o.date,
+    o.order_id,
+    o.order_amount,
+    o.region,
+    o.items_delivered,
+    o.items_missing,
+    o.delivery_hour,
+    c.customer_id,
+    c.customer_name,
+    c.customer_age,
+    d.driver_id,
+    d.driver_name,
+    d.age AS driver_age,
+    d.trips AS driver_trips
+FROM orders_data o
 JOIN customers_data c ON c.customer_id = o.customer_id
-JOIN drivers_data d ON d.driver_id = o.driver_id) t1
+JOIN drivers_data d ON d.driver_id = o.driver_id;
